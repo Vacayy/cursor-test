69be3eb57a0d808c0863dc530323bd0325f0fa27
Vacayy <juyeong.kim.201@gmail.com>
2024-11-10 22:38:32 +0900

    feat: 기본 세팅, recoil uninstall

--- playground/src/app/main/page.tsx
+++ playground/src/app/main/page.tsx
@@ -5,17 +5,17 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
 
-  const routeToMain = () => {
-    router.push("/main");
+  const routeToHome = () => {
+    router.push("/");
   }
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Button
         variant="default"
-        onClick={routeToMain}
+        onClick={routeToHome}
       >
-        Start
+        Home
       </Button>
     </div>
   );
